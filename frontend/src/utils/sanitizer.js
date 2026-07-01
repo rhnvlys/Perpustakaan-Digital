d0e2099fe6e7dfc4ed965321d34cefb9c468c6a3
Muhammad Zulfa Septiawan <2303010122@unper.ac.id>
2026-07-01 21:19:44 +0700

    feat(frontend): add html sanitization utility

---
+++ frontend/src/utils/sanitizer.js
@@ -0,0 +1,8 @@
+export function escapeHTML(value) {
+    return String(value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
